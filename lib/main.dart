a44104ba1a3e467791a0081bbe7dde7f45dfec12
FlutLab.io <>
2021-08-17 10:20:59 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,11 @@ void main() => runApp(
       MaterialApp(
         home: Scaffold(
           backgroundColor: Colors.amberAccent,
-          appBar: AppBar(
-            title: Text('I Am Rich'),
-            backgroundColor: Colors.redAccent,
+          appBar: Center(
+            child: AppBar(
+              title: Text('I Am Rich'),
+              backgroundColor: Colors.redAccent,
+            ),
           ),
           body: Image(
             image: NetworkImage('https://media-exp1.licdn.com/dms/image/C4D03AQEdHrby2IzlZg/profile-displayphoto-shrink_400_400/0/1516542582287?e=1634774400&v=beta&t=6greFQvped7cSa0ayFxSxPuAOKAmqV5xhfrdnKYOHI4'),
