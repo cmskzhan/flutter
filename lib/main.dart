1a7d39e6071976e67066dd890f112f537c39fee1
cmskzhan <kaizhang@hotmail.com>
2021-08-20 16:06:03 +0100

    one container

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,44 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.amberAccent,
-          appBar: AppBar(
-            title: Text('I Am Rich'),
-            backgroundColor: Colors.redAccent,
-            leading: const IconButton(
-              onPressed: null,
-              icon: Icon(Icons.menu),
-              tooltip: 'Menu',
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.amberAccent,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.redAccent,
+          leading: const IconButton(
+            onPressed: null,
+            icon: Icon(Icons.menu),
+            tooltip: 'Menu',
+          ),
+        ),
+        body: Center(
+          child: SafeArea(
+            child: Container(
+              color: Colors.white,
+              child: Text("body -> child's \n 2nd line"),
+              height: 100,
+              width: 150,
+              margin: EdgeInsets.only(left:100), // how much away from Center
+              padding: EdgeInsets.all(20), // text position inside the safearea
             ),
           ),
-          body: Center(
-            child: Text(
-              "Where am I?",
-              textDirection: TextDirection.rtl,
-            ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            tooltip: 'Add',
-            child: Icon(Icons.add_a_photo),
-            onPressed: () { print("Pressed");}, // this only update in debug console
-          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          tooltip: 'Add',
+          child: Icon(Icons.add_a_photo),
+          onPressed: () {
+            print("Pressed");
+          }, // this only update in debug console
         ),
       ),
     );
+  }
+}
