ce00825ae2ca3cc051c9026c781f266438cdbb53
cmskzhan <kaizhang@hotmail.com>
2021-09-08 09:41:08 +0100

    use packages and functions

--- lib/main.dart
+++ lib/main.dart
@@ -11,28 +11,36 @@ class XylophoneApp extends StatelessWidget {
                 player.play('note$tonation.wav');
   }
 
+  Expanded buildKeyboard({Color bgcolor = Colors.black, int wavefile = 1}) {
+    return Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    primary: Colors.amberAccent,
+                    backgroundColor: bgcolor,
+                  ),
+                  onPressed: () {
+                    playAsound(wavefile);
+                }, child: Text('play me doe'),),
+              );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              TextButton(
-                style: TextButton.styleFrom(
-                  primary: Colors.amberAccent,
-                  backgroundColor: Colors.red,
-                ),
-                onPressed: () {
-                  playAsound(1);
-              }, child: Text('play me doe'),),
+              buildKeyboard(),
+              buildKeyboard( bgcolor: Colors.blueAccent, wavefile: 3), //note the parameter use : instead of = like python
               TextButton(
                  style: TextButton.styleFrom(
                   primary: Colors.blue
                 ),
                 onPressed: () {
                 final player = AudioCache();
-                player.play('note2.wav');
+                player.play('note4.wav');
               }, child: Text('play me ray'),)
             ],
           ),
