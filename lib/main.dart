9831451d5dfa83c7435df5f2215298137f9c1f10
cmskzhan <kaizhang@hotmail.com>
2021-09-09 16:38:49 +0100

    track and display questions after clicks

--- lib/main.dart
+++ lib/main.dart
@@ -29,10 +29,20 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Widget> scoreKeeper = [
+  //create 2 lists, empty one to keepscore, a list of right and wrong icons to add to keepscore
+  List<Widget> score = [
     Icon(Icons.check, color: Colors.green),
     Icon(Icons.close, color: Colors.red)
   ];
+  List<Widget> scoreKeeper = [];
+
+  //create question list
+  List questions  = ['You can lead a cow down stairs but not up stairs.', 
+                    'Approximately one quarter of human bones are in the feet.',
+                    'A slug\'s blood is green.'];
+  int questionnumber = 0;
+
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -45,7 +55,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionnumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -70,13 +80,16 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                setState(() {
-                  scoreKeeper.add(Icon(
+                setState(() { 
+                  scoreKeeper.add(Icon( //add tick  to scoreKeeper list hard way
                     Icons.check,
                     color: Colors.green,
                   ));
                 });
                 print("true pressed");
+
+                questionnumber++;
+
               },
             ),
           ),
@@ -96,7 +109,11 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                setState(() { // add x to scoreKeeper list from score list
+                  scoreKeeper.add(score[1]);
+                });
                 print("pressed false!");
+                questionnumber++;
               },
             ),
           ),
